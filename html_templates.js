d912163568fd357fa433b471f665358bd61caa46
DasBaka <thebakalestear@gmail.com>
2023-02-01 14:50:18 +0100

    added timer (missing function)

--- html_templates.js
+++ html_templates.js
@@ -1,24 +1,48 @@
 //templates
-let settingsTemp = /*html*/`
+let settingsTemp = /*html*/ `
 <fieldset class="pb-3 contentWidth d-flex flex-column align-items-center">
     <button type="button" class="btn hover w-75 px-5 py-3 mx-2 my-2 btn-secondary " id="typeSetting" onclick="toggleType()">Quiz-Type: Flags -> Country</button>
-    <input class="w-50 mt-3" type="range" name="questionAmount" onload="getRangeValue()" oninput="getRangeValue()" id="questionAmount" step="1" min="1" max="100"><label for="questionAmount" id="rangeLabel">Questions: 50</label>
+    <input class="w-75 mt-3" type="range" name="questionAmount" onload="getRangeValue()" oninput="getRangeValue()" id="questionAmount" step="1" min="1" max="100"><label for="questionAmount" id="rangeLabel">Questions: 50</label>
 </fieldset>
-<button type="button" class="btn hover w-50 contentWidth px-5 py-3 mx-2 my-2 btn-outline-secondary" onclick="startQuiz()">Let's Guess!</button> `;
+<button type="button" class="btn hover contentWidth px-5 py-3 mx-2 my-2 btn-outline-secondary" onclick="startQuiz(), saveValue()">Let's Guess!</button> `;
 
-let startTemp = /*html*/`
+let startTemp = /*html*/ `
     <button type="button" class="btn hover contentWidth px-5 py-3 mx-2 my-2 btn-outline-primary" onclick="screenChange(settingsTemp, 'btnDiv' )">Start</button>`;
 
-const countdown = ["3", "2", "1", "&#9872;"];
+const countdown = ['3', '2', '1', '&#9872;'];
 
 function answerTemplate(x) {
-    let answers = "";
-    for (let i = 0; i < 4; i++) {
-     answers += /*html*/`<button type="button" class="btn hover contentWidth px-5 py-3 mx-2 my-2 btn-outline-primary" onclick="checkAnswer(${i}), disableAll()" id="answer${i}" >${x[i]}</button>`;
-    };
-    return answers;
+  let answers = '';
+  for (let i = 0; i < 4; i++) {
+    answers += /*html*/ `<button type="button" class="btn hover contentWidth px-5 py-3 mx-2 my-2 btn-outline-primary" onclick="checkAnswer(${i}), disableAll(), stopTimer()" id="answer${i}" >${x[i]}</button>`;
+  }
+  return answers;
 }
 
 function questionTemplate(x) {
-   return /*html*/ `<img class="flag-image" src="flags/${x}" />`;
+  return /*html*/ `<img class="flag-image" src="flags/${x}" />`;
+}
+
+let timeTemplate = /*html*/ `
+    <div id="footR1">Remaining Time:</div>
+    <div id="timeSec"></div>
+    <div id="footR2">sec</div>`;
+
+//implement templates & changing Screen (Div)
+function loadTemp(template, container) {
+  document.getElementById(container).innerHTML = '';
+  document.getElementById(container).innerHTML = template;
+}
+
+function screenChange(template, container) {
+  let div = document.getElementById(container);
+  div.style.opacity = 0;
+  setTimeout(function () {
+    loadTemp(template, container);
+    div.style.opacity = 1;
+  }, 250);
+}
+
+function hideInnerHTML(container) {
+  document.getElementById(container).innerHTML = '';
 }
--- script.js
+++ script.js
@@ -1,172 +1,166 @@
 createFlagsArray();
-var currentType = "flag";
-var currentTypeInvert = "country";
+var currentType = 'flag';
+var currentTypeInvert = 'country';
 var currentQuestion = 0;
-
-//implement templates & changing Screen (Div)
-function loadTemp(template, container) {
-   document.getElementById(container).innerHTML = "";
-   document.getElementById(container).innerHTML = template;
-}
-
-function screenChange(template, container) {
-    let div = document.getElementById(container);
-    div.style.opacity = 0;
-    setTimeout(function(){
-        loadTemp(template, container);
-        div.style.opacity = 1;}
-        , 250);
-}
-
-function hideInnerHTML(container) {
-    document.getElementById(container).innerHTML = "";
-}
+let questionAmount = '';
 
 //onload
 function createFlagsArray() {
-    for (let i = 0; i < countries.length; i++) {
-        countries[i]["answers"] = generateAnswers(countries[i]["country"]);
-    }
-    shuffleArray(countries);
-    console.log(countries);
+  for (let i = 0; i < countries.length; i++) {
+    countries[i]['answers'] = generateAnswers(countries[i]['country']);
+  }
+  shuffleArray(countries);
+  console.log(countries);
 }
 
 function generateAnswers(country) {
-    let answers = [country];
-    for(let i = 1; i < 4; i++){
-        let falseAnswer = countries[randomArrayIndex(countries)]["country"];
-        while (answers.includes(falseAnswer)) {
-            falseAnswer = countries[randomArrayIndex(countries)]["country"];
-        }
-        answers.push(falseAnswer);
+  let answers = [country];
+  for (let i = 1; i < 4; i++) {
+    let falseAnswer = countries[randomArrayIndex(countries)]['country'];
+    while (answers.includes(falseAnswer)) {
+      falseAnswer = countries[randomArrayIndex(countries)]['country'];
     }
-    shuffleArray(answers);
-    return answers;
+    answers.push(falseAnswer);
+  }
+  shuffleArray(answers);
+  return answers;
 }
 
 function resetAll() {}
 
 //shuffle and randomize
 function randomArrayIndex(array) {
-    let index = Math.floor(Math.random()*array.length);
-    return index;
+  let index = Math.floor(Math.random() * array.length);
+  return index;
 }
 
 function shuffleArray(array) {
-    for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[j]; //switches position
-        array[j] = temp;
-    }
-    return array;
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j]; //switches position
+    array[j] = temp;
+  }
+  return array;
 }
 
 //settings
 function toggleType() {
-    let toggle = document.getElementById("typeSetting");
-    switch (currentType) {
-        case "flag": 
-            currentTypeInvert = currentType;
-            toggle.innerHTML = /*html*/`<b>Quiz-Type: Country -> Flags</b>`
-            currentType = "country";
-            break;
-        case "country": 
-            currentTypeInvert = currentType;
-            toggle.innerHTML = /*html*/`<b>Quiz-Type: Flags -> Country</b>`
-            currentType = "flag";
-            break;
-    }
+  let toggle = document.getElementById('typeSetting');
+  switch (currentType) {
+    case 'flag':
+      currentTypeInvert = currentType;
+      toggle.innerHTML = /*html*/ `<b>Quiz-Type: Country -> Flags</b>`;
+      currentType = 'country';
+      break;
+    case 'country':
+      currentTypeInvert = currentType;
+      toggle.innerHTML = /*html*/ `<b>Quiz-Type: Flags -> Country</b>`;
+      currentType = 'flag';
+      break;
+  }
 }
 
 function getRangeValue() {
-    let amount = document.getElementById("rangeLabel");
-    let val = document.getElementById("questionAmount").value;
-    amount.innerHTML = `Questions: ` + Number(val);
+  let amount = document.getElementById('rangeLabel');
+  let val = document.getElementById('questionAmount').value;
+  amount.innerHTML = `Questions: ` + Number(val);
 }
 
 //start quiz
 function startQuiz() {
-    screenChange("Get ready...","questionText");
-    screenChange("","btnDiv");
-    countdownTimer();
+  screenChange('Get ready...', 'questionText');
+  screenChange('', 'btnDiv');
+  countdownTimer();
 }
 
 function countdownTimer() {
-    let i = 0;
-    var startInterval = setInterval(function(){
-        screenChange(countdown[i], 'screen')
-        if(i < countdown.length){
-            i++} else {
-                clearInterval(startInterval);
-                nextQuestion();
-            };}
-        , 1000);
+  let i = 0;
+  var startInterval = setInterval(function () {
+    screenChange(countdown[i], 'screen');
+    if (i < countdown.length) {
+      i++;
+    } else {
+      clearInterval(startInterval);
+      nextQuestion();
+    }
+  }, 1000);
 }
 
 //check answer
 function checkAnswer(id) {
-    if (countries[currentQuestion]["country"] == document.getElementById('answer' + id).innerHTML) {
-        rightAnswer(id);
-        prepareNext();
-    } else {
-        wrongAnswer(id);
-        prepareNext()
-    }
+  if (countries[currentQuestion]['country'] == document.getElementById('answer' + id).innerHTML) {
+    rightAnswer(id);
+  } else {
+    wrongAnswer(id);
+  }
+  prepareNext();
 }
 
 function rightAnswer(id) {
-    let answer = document.getElementById('answer' + id);
-    const classes = ["btn-success", "btn-outline-primary", "hover"];
-    classes.forEach(element => {
-        toggleIfRight(answer, element);
-    });
+  let answer = document.getElementById('answer' + id);
+  const classes = ['btn-success', 'btn-outline-primary', 'hover'];
+  classes.forEach((element) => {
+    toggleIfRight(answer, element);
+  });
 }
 
-function toggleIfRight(answer, element){
-    if(answer.classList.contains(element)){
-        answer.classList.remove(element);
-    } else {
-        answer.classList.add(element);
-    }        
+function toggleIfRight(answer, element) {
+  if (answer.classList.contains(element)) {
+    answer.classList.remove(element);
+  } else {
+    answer.classList.add(element);
+  }
 }
 
 function wrongAnswer(id) {
-    let answer = document.getElementById('answer' + id);
-    const classes = ["btn-danger", "btn-outline-primary", "hover"];
-    classes.forEach(element => {
-       toggleIfWrong(answer, element);
-    });
-    rightAnswer(countries[currentQuestion]["answers"].indexOf(countries[currentQuestion]["country"]));
+  let answer = document.getElementById('answer' + id);
+  const classes = ['btn-danger', 'btn-outline-primary', 'hover'];
+  classes.forEach((element) => {
+    toggleIfWrong(answer, element);
+  });
+  if (timesUp == false) {
+    rightAnswer(countries[currentQuestion]['answers'].indexOf(countries[currentQuestion]['country']));
+  }
 }
 
-function toggleIfWrong(answer, element){
-    if(answer.classList.contains(element)){
-        answer.classList.remove(element);
-    } else {
-        answer.classList.add(element);
-    }      
+function toggleIfWrong(answer, element) {
+  if (answer.classList.contains(element)) {
+    answer.classList.remove(element);
+  } else {
+    answer.classList.add(element);
+  }
 }
 
 function disableAll() {
-    for (let i = 0; i < 4; i++) {
-        document.getElementById('answer' + i).disabled = true;
-    }
+  for (let i = 0; i < 4; i++) {
+    document.getElementById('answer' + i).disabled = true;
+  }
 }
 
 //next answer
 function nextQuestion() {
+  if (currentQuestion == questionAmount) {
+    console.log('ENDE');
+  } else {
     screenChange(questionTemplate(countries[currentQuestion][currentType]), 'screen');
-    screenChange(answerTemplate(countries[currentQuestion]["answers"]), 'btnDiv');
-    screenChange("What's the " + currentTypeInvert + " to this " + currentType + "?","questionText");
+    screenChange(answerTemplate(countries[currentQuestion]['answers']), 'btnDiv');
+    screenChange("What's the " + currentTypeInvert + ' to this ' + currentType + '?', 'questionText');
+    startTimer();
+  }
 }
 
 async function prepareNext() {
-    currentQuestion += 1;
-    await wait(2500);
-    nextQuestion();
+  currentQuestion += 1;
+  await wait(2000);
+  nextQuestion();
+  timesUp = false;
 }
 
 function wait(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+function saveValue() {
+  questionAmount = document.getElementById('questionAmount').value;
+}
